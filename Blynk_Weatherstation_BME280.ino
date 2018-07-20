0b0a47f867baa1349e1857195caf349612662b75
zenpa49 <41239421+zenpa49@users.noreply.github.com>
2018-07-20 17:13:46 +0200

    Add files via upload

---
+++ Blynk_Weatherstation_BME280.ino
@@ -0,0 +1,65 @@
+//HF 20 7 2018, With Bridge to OLED
+//Combined with Blynk_OLED_Weatherstation
+ 
+#define BLYNK_PRINT Serial
+#define LED D0 
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <Adafruit_Sensor.h>  //sensors
+#include <Adafruit_BME280.h>  //barometer
+#include <math.h>
+
+char auth[] = "YourAuthToken";
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+char oledauth[] = "YourDeviceAuthToken";//token OLED device 2
+
+//variabelen
+float vochtigheid;
+float temperatuur;
+float luchtdruk;
+Adafruit_BME280 bme;//I2C address 0x76, Attention: change in library-updates !!
+WidgetBridge bridge1(V4);//bridge definition
+
+BlynkTimer timer;
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  bme.begin();
+  pinMode(LED, OUTPUT);   // LED pin as output.
+  timer.setInterval(5000L, sendSensor);
+}//setup()
+
+//initialise Bridge
+BLYNK_CONNECTED() {
+  bridge1.setAuthToken(oledauth); // Token of device 2
+}
+
+void sendSensor()
+{
+  digitalWrite(LED, LOW);   // turn the LED on
+  vochtigheid = bme.readHumidity();
+  temperatuur = bme.readTemperature();
+  luchtdruk = bme.readPressure()/100;//correction for hPascal
+  
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V1, temperatuur);//for use on app
+  Blynk.virtualWrite(V2, vochtigheid);//for use on app
+  Blynk.virtualWrite(V3, luchtdruk);//for use on app
+  bridge1.virtualWrite(V4, temperatuur, vochtigheid, luchtdruk); //and send it to device 2 via bridge1 as V4
+  digitalWrite(LED, HIGH);// turn the LED off
+}//sendSensor()
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}//loop()
+
---
+++ Blynk_Weatherstation_OLED-display.ino
@@ -0,0 +1,63 @@
+//OLED display op NODEMCU V2, SDK->D1 SDA->D2, 3,3V
+//HF 20 juli 2018, 
+//Combined with Blynk_Weatherstation_BME280, driven via Blynk-Bridge
+//Keep project active to preserve AuthToken for this device!!! Make a placeholder Project
+
+#define BLYNK_PRINT Serial
+#include <BlynkSimpleEsp8266.h>
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <ESP_SSD1306.h>    // Modification of Adafruit_SSD1306 for ESP8266 compatibilit
+#define OLED_RESET 16
+ESP_SSD1306 display(OLED_RESET); // FOR I2C
+
+char auth[] = "YourAuthToken";//Keep project active to preserve this AuthToken for the device!!!
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+char deg = 247;
+
+const int t=5000;
+String tempt,druk,vocht;
+
+void setup()   {               
+Serial.begin (9600);
+  delay(1000);
+  // init OLED display
+  Serial.print("setup OLED display");
+  display.begin(SSD1306_SWITCHCAPVCC);  // Switch OLED (aangepaste versie voor ESP8266)
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.display();
+  delay(1000);
+  // Clear the buffer.
+  display.clearDisplay();
+  delay(1000);
+  // start Wifi
+  Blynk.begin(auth, ssid, pass);
+  delay(2000);
+}//setup
+
+//data via bridge naar display
+BLYNK_WRITE(V4)//from Blynk_Weatherstation_BME280
+  {
+    tempt = String (param[0].asFloat(),1); // assigning incoming temperature value from pin V4 to tempt
+    druk = String (param[1].asFloat(),1); // assigning incoming pressure value from pin V4 to druk
+    vocht = String (param[2].asFloat(),1); // assigning incoming humidity value from pin V4 to vocht
+    
+    //prepare OLED:(x pixels = [0-128] ,y pixels = [0-64])0,0 is upperleft   
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    //prepare Data
+    display.setCursor(0,2);display.print("Temperature: "+tempt+" "+char(247)+"C");
+    display.setCursor(0,16);display.println("Pressure:   "+druk+" hPa");
+    display.setCursor(0,29);display.println("Humidity: "+vocht+" %");
+    //put on OLED display
+    display.display();
+  }//BLYNK_WRITE(V4)
+
+void loop() {
+  Blynk.run();
+}//loop
+
